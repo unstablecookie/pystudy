27dce836203d07e282801c9fd17afec6a34ae630
unknown <realxellor@gmail.com>
2020-10-14 00:32:04 +0300

    a bit more tests

--- TDD/sample1.py
+++ TDD/sample1.py
@@ -1,10 +1,26 @@
 import unittest
 from keks import keks
+from wut import Wut
 
 class KeksTest(unittest.TestCase):
 	def test_createkeks(self):
 		kek = keks(8)
 		self.assertEqual(kek.number,8)
+
+
+
+class TopsTest(unittest.TestCase):
+	def test_gettops(self):
+		wut = Wut()
+		tops = wut.get_gettops()
+		self.assertCountEqual(tops,[])
+	def test_add_tops(self):
+		wut = Wut()
+		kek = keks(123)
+		wut.addkeks(kek)
+		tops = wut.get_gettops()
+
+		self.assertEqual(tops[0],kek)
 
 if __name__ == '__main__':
 	unittest.main()
---
+++ TDD/wut/__init__.py
@@ -0,0 +1,9 @@
+class Wut:
+	def __init__(self):
+		self.tops = []
+
+	def get_gettops(self):
+		return self.tops
+
+	def addkeks(self,kek):
+		self.tops.append(kek)
